d35e038c88dab2a6f0a12d56d76b79510f983e89
Robert Pack <robstar.pack@gmail.com>
2023-09-21 17:25:48 +0200

    feat: render catalog tree

--- app/src/app/query/Sidebar/TableTree.tsx
+++ app/src/app/query/Sidebar/TableTree.tsx
@@ -1,17 +1,36 @@
 "use client";
 
-import { makeStyles, tokens } from "@fluentui/react-components";
+import { useState, useEffect } from "react";
+import {
+  makeStyles,
+  tokens,
+  HeadlessFlatTreeItemProps,
+  useHeadlessFlatTree_unstable,
+  useRestoreFocusTarget,
+  TreeItemLayout,
+  FlatTree,
+  Menu,
+  MenuPopover,
+  MenuItem,
+  MenuTrigger,
+  MenuList,
+  FlatTreeItem,
+} from "@fluentui/react-components";
 import { useSuspenseQuery } from "@suspensive/react-query";
 import { tableFromIPC, Table, Utf8, Binary } from "apache-arrow";
 
 import { getTables } from "@/gen";
 
+type FlatItem = HeadlessFlatTreeItemProps & { content: string };
+
 const useStyles = makeStyles({
   root: {
-    display: "flex",
-    flexDirection: "row",
-    flexGrow: 1,
-    backgroundColor: tokens.colorNeutralBackground3,
+    height: "100%",
+    width: "100%",
+  },
+  tree: {
+    height: "100%",
+    width: "100%",
   },
 });
 
@@ -23,22 +42,99 @@ type tablesSchema = {
   table_schema: Binary;
 };
 
-const useCatalogs = (): Table<tablesSchema> => {
+const useTreeItems = (): FlatItem[] => {
+  const [flatTree, setFlatTree] = useState<FlatItem[]>([]);
   const { data } = useSuspenseQuery(
     getTables.useQuery({ includeSchema: true })
   );
-  const table = tableFromIPC(data.data);
-  return table;
+
+  useEffect(() => {
+    const table = tableFromIPC<tablesSchema>(data.data);
+
+    const catalogs: Record<string, FlatItem> = {};
+    const schemas: Record<string, FlatItem> = {};
+    const items: FlatItem[] = [];
+
+    for (let i = -1, n = table.numRows; ++i < n; ) {
+      const row = table.get(i);
+      if (!row) {
+        continue;
+      }
+      if (!(row.catalog_name in catalogs)) {
+        catalogs[row.catalog_name] = {
+          value: row.catalog_name,
+          content: row.catalog_name,
+        };
+      }
+
+      const schemaValue = `${row.catalog_name}.${row.db_schema_name}`;
+      if (!(schemaValue in schemas)) {
+        schemas[schemaValue] = {
+          value: schemaValue,
+          content: row.db_schema_name,
+          parentValue: row.catalog_name,
+        };
+      }
+
+      const tableValue = `${schemaValue}.${row.table_name}`;
+      items.push({
+        value: tableValue,
+        content: row.table_name,
+        parentValue: schemaValue,
+      });
+
+      const schema = tableFromIPC(row.table_schema).schema;
+      for (const field of schema.fields) {
+        items.push({
+          value: `${tableValue}.${field.name}`,
+          content: field.name,
+          parentValue: tableValue,
+        });
+      }
+
+      setFlatTree([
+        ...Object.values(catalogs),
+        ...Object.values(schemas),
+        ...items,
+      ]);
+    }
+  }, [data]);
+
+  return flatTree;
 };
 
 export function TableTree(): JSX.Element {
   const classes = useStyles();
-  const table = useCatalogs();
+  const treeItems = useTreeItems();
+  const flatTree = useHeadlessFlatTree_unstable(treeItems);
+  const focusTargetAttribute = useRestoreFocusTarget();
 
-  const row = table.get(1);
-  const schema = tableFromIPC(row?.table_schema);
-
-  console.log({ schema });
-
-  return <main className={classes.root}>Hello World</main>;
+  return (
+    <div className={classes.root}>
+      <FlatTree {...flatTree.getTreeProps()} aria-label="Flat Tree">
+        {Array.from(flatTree.items(), (flatTreeItem) => {
+          const { content, ...treeItemProps } = flatTreeItem.getTreeItemProps();
+          return (
+            <Menu
+              key={flatTreeItem.value}
+              positioning="below-end"
+              openOnContext
+            >
+              <MenuTrigger disableButtonEnhancement>
+                <FlatTreeItem {...focusTargetAttribute} {...treeItemProps}>
+                  <TreeItemLayout>{content}</TreeItemLayout>
+                </FlatTreeItem>
+              </MenuTrigger>
+              <MenuPopover>
+                <MenuList>
+                  <MenuItem>Edit</MenuItem>
+                  <MenuItem>New</MenuItem>
+                </MenuList>
+              </MenuPopover>
+            </Menu>
+          );
+        })}
+      </FlatTree>
+    </div>
+  );
 }
